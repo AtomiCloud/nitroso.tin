53517a43bac0861231e973aafdc4f3156cab1529
kirinnee <kirinnee97@gmail.com>
2024-05-29 21:46:59 +0800

    feat: pin commit-analyzer

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 rm .git/hooks/*
-npm i conventional-changelog-conventionalcommits@7.0.2 @semantic-release/release-notes-generator@12.0.0
+npm i -D conventional-changelog-conventionalcommits@7.0.2 @semantic-release/release-notes-generator@12.0.0 @semantic-release/commit-analyzer@12.0.0
 sg release -i npm || true
