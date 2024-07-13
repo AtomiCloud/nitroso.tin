3af0a96152c1280b1bb22cdb8a7158245236eb8c
kirinnee <kirinnee97@gmail.com>
2024-07-13 19:45:28 +0800

    dep: update semantic-release to 0.9.0

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs.url = "nixpkgs/d816b5ab44187a2dd84806630ce77a733724f95f";
     nixpkgs-2305.url = "nixpkgs/nixos-23.05";
     nixpkgs-feb-23-24.url = "nixpkgs/0e74ca98a74bc7270d28838369593635a5db3260";
-    atomipkgs.url = "github:kirinnee/test-nix-repo/v23.0.1";
+    atomipkgs.url = "github:kirinnee/test-nix-repo/v27.0.0";
   };
   outputs =
     { self
--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 rm .git/hooks/*
-npm i -D conventional-changelog-conventionalcommits@7.0.2 @semantic-release/release-notes-generator@12.0.0 @semantic-release/commit-analyzer@12.0.0
 sg release -i npm || true
