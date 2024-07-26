d215dd2e70bf639c0473331d64f9c91580428db9
kirinnee <kirinnee97@gmail.com>
2024-07-26 14:12:06 +0800

    feat: improve proxy-processing script

--- scripts/local/process_proxy.sh
+++ scripts/local/process_proxy.sh
@@ -3,7 +3,8 @@
 # URL of the endpoint
 endpoint_url="$1"
 
-rm proxy.txt
+rm proxy.txt || true
+rm proxy.tmp || true
 
 curl -s "$endpoint_url" | while IFS= read -r line; do
   # Extract each part of the proxy string
@@ -14,6 +15,27 @@ curl -s "$endpoint_url" | while IFS= read -r line; do
 
   url="http://${user}:${password}@${ip}:${port};"
   # Concatenate into the new format
-  printf "%s" "${url}" >>proxy.txt
-
+  printf "%s" "${url}" >>proxy.tmp
 done
+
+# Read the content of the file into a variable
+input_string=$(cat ./proxy.tmp)
+
+# Set the Internal Field Separator to ';'
+IFS=';' read -r -a entries <<<"$input_string"
+
+# Join the first 200 entries into a variable `a`
+a=$(
+  IFS=';'
+  echo "${entries[*]:0:200}"
+)
+
+# Join the remaining 300 entries into a variable `b`
+b=$(
+  IFS=';'
+  echo "${entries[*]:200:300}"
+)
+
+# Print the variables if you want to check
+printf "helium: %s\n" "${a}" >>proxy.txt
+printf "tin: %s\n" "${b}" >>proxy.txt
